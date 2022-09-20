dfe24baa219877c4fe6484e01e0c77f8546d2305
deepanshu tomar <deepanshutomar@Deepanshus-MacBook-Pro.local>
2022-09-20 22:31:01 +0530

    base case

---
+++ index.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const socketIO = require('socket.io');
+// const { Socket } = require('dgram');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+const users = [{}];
+
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('server is working');
+});
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: 'http://localhost:3001',
+  },
+});
+io.on('connection', (socket) => {
+  console.log('New Connection');
+
+  socket.on('joined', ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit('userJoined', {
+      user: 'Admin',
+      message: `${users[socket.id]} has joined`,
+    });
+    socket.emit('welcome', {
+      user: 'Admin',
+      message: `welcome to the chat,${users[socket.id]} `,
+    });
+    socket.on('message', ({current,message, id}) => {
+      console.log(current,message,id);
+      io.emit('sendMessage', { current, message, id });
+    });
+  });
+
+  socket.on('disconnected', () => {
+    socket.broadcast.emit('leave', {
+      user: 'Admin',
+      message: `${users[socket.id]} has left`,
+    });
+    console.log('user left');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server is connected on http://localhost:${port}`);
+});
